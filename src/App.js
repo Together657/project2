c5ec8c5cd39ee2e2f8c6a7904cb3a20e396bddf9
dipesh pathak <pathakdipesh657@gmail.com>
2022-10-14 13:14:59 +0530

    create new repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import About from "./Components/Pages/About";
-import Login from "./Components/Pages/auth/Login";
-import Home from "./Components/Pages/Home";
-import Layout from "./Components/Pages/Layout";
-// import Profile from './Components/Pages/auth/Profile';
+import './App.css';
+import {  Route, Routes } from 'react-router-dom';
+import { SignUp } from './components/Signup';
+import { SignIn } from '../src/components/SignIn';
+// import { Switch } from '@material-ui/core';
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Login />} />
-            <Route path="about" element={<About />} />
-            <Route path="home" element={<Home />} />
-            {/* <Route path="profile" element={<Profile />}/> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    
+        <Route exact path='/' Components={SignIn}/>
+         <Route exact path='SignUp' Components={SignUp }/>
+      </>
+
   );
 }
 
